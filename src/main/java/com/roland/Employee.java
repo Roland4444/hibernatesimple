fc18e60d575c0d246d7c248315890e62bb5d9571
Roland4444 <turbopollux@rambler.ru>
2020-10-27 12:30:51 +0400

    up

---
+++ src/main/java/com/roland/Employee.java
@@ -0,0 +1,30 @@
+package com.roland;
+public class Employee {
+    private int id;
+    private String firstName, secondname;
+
+    public int getId() {
+        return id;
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public void setSecondname(String secondname) {
+        this.secondname = secondname;
+    }
+}
